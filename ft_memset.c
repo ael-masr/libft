cffa44929d14e1bcfb6f05820c0211e37c9b2a81
ael-masr <abdulelmasri96@gmail.com>
2022-02-24 22:04:37 +0400

    25/2

--- ft_memset.c
+++ ft_memset.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *b, int c, size_t len)
+/*void	*ft_memset(void *b, int c, size_t len)
 {
 	char	*p;
 
@@ -20,5 +20,19 @@ void	*ft_memset(void *b, int c, size_t len)
 		p[len - 1] = c;
 		len--;
 	}
+	return (b);
+}*/
+
+include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char *)b;
+	while (i < len)
+		ptr[i++] = (unsigned char)c;
 	return (b);
 }
